44f2935f0ef51c04c24fed786a1cbadb77e5e897
Ahmed Walid <66032437+ahmeddwalid@users.noreply.github.com>
2024-05-08 16:25:43 +0300

    Added Javascript

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-function login(){
-    var user = document.myform.email.value ;
-    var pass =  document.myform.password.value ;
-    var a = "AHMED@GMAIL.COM";
+function login() {
+  var user = document.myform.email.value;
+  var pass = document.myform.password.value;
+  var a = "AHMED@GMAIL.COM";
 
-    if(user.toUpperCase() == a&& pass.toUpperCase() == a){
-        window.location.href = "index.html";
-    }else{
-        window.alert("Error in the user name or password");
+  if (user.toUpperCase() == a && pass.toUpperCase() == a) {
+    window.location.href = "index.html";
+  } else {
+    window.alert("Error in the user name or password");
+  }
+}
+
+function openmenu() {
+  var menu = document.getElementById("menu");
+  menu.style.width = "240px";
+}
+
+function closemenu() {
+  var menu = document.getElementById("menu");
+  menu.style.width = "0px";
+}
+
+let i=1;
+
+function Next() {
+
+    if(i==4){
+      i=1;
     }
+    else{
+      i++
+    }
+    document.getElementById("slideshow").src= "assets/images/slideShow/"+i+".jpg";
 }
 
-
-function openmenu(){
-    var menu =  document.getElementById("menu");
-    menu.style.width = "240px";
-}
-
-
-function closemenu(){
-    var menu =  document.getElementById("menu");
-    menu.style.width = "0px";
-}
-
-
-
+setInterval(Next,2000);
